5fe85b161b008cbc9cc98410f23dc5f274a93269
George Karavaev <shadowp@gmail.com>
2020-03-30 16:47:32 -0700

    update manage4.ns -- add an option to BUY AND MANAGE SERVERS OMG
    so we can easily add this to our pool, like os managed pool

--- manage4.js
+++ manage4.js
@@ -14,6 +14,90 @@ function loadcfg(ns, config) {
     }
     return cfg
 }
+const PURCHASE_NAME = "DO_NOT_TOUCH_SERVER"
+function buy_biggest_server(ns,server_list)
+{
+    var money_available = ns.getServerMoneyAvailable("home") - 1000000;
+    //note we check game to see if we have max server, not our app, we good neighborgs
+    //cost per 1GB is 55k, so money_available / 55k =>num of cores we can get, and we need to round down to pow of 2
+    var largest_memory_buyable=(Math.pow(2,Math.floor(Math.log(money_available/55000)/Math.log(2)))); //this is largest memory we can do
+    if(largest_memory_buyable > ns.getPurchasedServerMaxRam())
+        largest_memory_buyable = ns.getPurchasedServerMaxRam();
+    var new_server =ns.purchaseServer(PURCHASE_NAME, largest_memory_buyable);
+    if(new_server !== "")
+    {
+        ns.tprint("Bought new server with RAM " + largest_memory_buyable)
+            var new_server_object = {server_ram:largest_memory_buyable, state:"idle", waiting_to_sell:false};
+            server_list.set(new_server,new_server_object);
+    }
+    else
+    {
+        ns.tprint("UNABLE TO BUY NEW SERVER!! RAM req "+largest_memory_buyable)
+    }
+    //woooo we buyed it
+    //or not..... no errors!
+    return;
+}
+
+/// SERVER:
+///    server_ram: TOTAL_RAM
+///    state: busy, idle, 
+///    WAITING_TO_SELL: true, false
+//Figures out if we can BUY MORE AND BETTER SERVERS
+function manage_servers(ns,server_list)
+{
+    var money_available = ns.getServerMoneyAvailable("home") - 1000000;
+    if(money_available <= 1000000) return; //early game, return if less then 1M (+1mil/server)
+    
+    //first we check if we are trying to sell a server, and what's the smallest server is.
+    var smallest_ram=13100000000000.0;
+    var smallest_server = "";
+    var waiting_to_sell = false;
+      
+    
+    for (var best_server of server_list)
+    {
+        if(best_server[1].server_ram < smallest_ram)
+        {
+            smallest_ram = best_server[1].server_ram
+            smallest_server = best_server[0]
+        }
+    }
+    
+    
+    if(smallest_server !== ""){
+    if(server_list.get(smallest_server).waiting_to_sell === true)
+    {
+        ns.tprint("Trying to delete + " + smallest_server)
+        var able_to_sell = ns.deleteServer(smallest_server)
+        if(able_to_sell===false) return; //we need to wait for that zombie to die first, can't do anything with it zombie
+        //else
+        //woo we sold it!
+        ns.print("Success deleting + " +smallest_server);
+        server_list.delete(smallest_server)
+    }}
+    
+    //if we have no servers then buy largest server we can
+    if(smallest_server === "")
+    {
+        buy_biggest_server(ns,server_list)
+        return; //we done boyz
+    }
+    //next we check what the smallest ram server cost.
+    //if our money is <2x the smallest ram server cost we do nothing
+    if(ns.getPurchasedServerCost(smallest_ram*2) > money_available)
+        return; // not enough money
+    //next we check if we CAN just buy a bigger server
+    if( ns.getPurchasedServers().length < (ns.getPurchasedServerLimit()-2))
+    {
+
+        buy_biggest_server(ns,server_list)
+        return
+    }
+    ns.print("Asking system to delete + " + smallest_server);
+    //if not we need to DELETE the smallest server 
+    server_list.get(smallest_server).waiting_to_sell = true;
+}
 
 export async function main(ns) {
     var schema = {
@@ -41,6 +125,26 @@ export async function main(ns) {
     }
 
     var time_to_load_config = 0
+    var owned_servers = new Map() //string as name, object as value
+    
+        var servers_bought = ns.getPurchasedServers()
+        for(var one_server of servers_bought)
+        {
+            if(one_server.length < PURCHASE_NAME.length)
+                continue; 
+            if( one_server.substring(0, PURCHASE_NAME.length) == PURCHASE_NAME)
+            {
+                //ns.tprint(one_server.substring(0,PURCHASE_NAME.length))
+                //steal the server 
+                var a = ns.getServerRam(one_server)[0];
+                var new_server_object = {"server_ram":a, "state":"idle", "waiting_to_sell":false};
+                owned_servers.set(one_server,new_server_object);
+                ns.killall(one_server)
+                //ns.tprint("stealing a server to use " + one_server)
+            }
+        }
+        ns.tprint("Stolen servers! "+owned_servers.size);
+    
     var jobs = {}
     do {
         var cur_time = ns.getTimeSinceLastAug()
@@ -59,6 +163,7 @@ export async function main(ns) {
                 var tmp_path = cfg.tmp_path
                 var growratio = cfg.growratio
                 var use_targets = cfg.use_targets || false
+                var bool_manage_servers = cfg.manage_servers || false
                 var home_reserve = cfg.home_reservation
             } catch (e) {
                 ns.tprint("Failed: " + e)
@@ -80,7 +185,6 @@ export async function main(ns) {
                 ns.write(sc_weak, "export async function main(ns) { await ns.weaken(ns.args[0]); }", "w")
             }
         }
-
         var ramlimits = {}
         if (home_reserve !== undefined) {
             var gsr = ns.getServerRam("home")
@@ -286,6 +390,12 @@ export async function main(ns) {
                 }
             }
         } catch (e) {}
+        
+        if(bool_manage_servers)
+        {
+            manage_servers(ns,owned_servers)    
+            //TBD NOW WE CAN GIVE IT TO RAMLIMITS
+        }
         var sleep_until = time_to_load_config
         for (var target in jobs) {
             var job = jobs[target]
